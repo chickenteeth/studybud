df675af39b3a7c253194f367339a7442543370ab
Tristan Stutzman <tristan.stutzman@gmail.com>
2022-01-13 21:35:40 -0700

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from opcode import hascompare
+from django.urls import path
+from base import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room')
+]
